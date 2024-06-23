dc9816b150a05953aa222584b8fec3eefeb15c6c
gustonstomy <gustomstomy@gmail.com>
2024-06-23 12:53:03 +0100

    initializing and creeating directories

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='main-container' >
+      <h1>Password-generator-app</h1>
+        
+    </div>
+  )
+}
+
+export default App
